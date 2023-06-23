8771e81311fec0475726d88b2b5222ad6e1cbfd8
Joanna Kolakowska <kolakowska.joanna.jk@gmail.com>
2023-06-23 22:37:54 +0200

    sound working

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import IMG1 from "./assets/a.png";
 import IMG2 from "./assets/b.png";
 import IMG3 from "./assets/c.png";
 import IMG4 from "./assets/d.png";
+import {Howl, Howler} from "howler"
+import BUBBLES from "./audio/coffee-makers-bubbling-32827.mp3";
 import "./App.css";
 import { useState } from "react";
+
+const audioClip = [
+  {sound: BUBBLES, label: "Kliknij mnie"}
+]
 
 function App() {
   const [imageLink, setImageLink] = useState(IMG1);
 
-  let handleClick = () => {
+ const soundPlay = (src) => {
+  const sound = new Howl({
+    src
+  })
+  sound.play();
+ }
+
+
+  const handleClick = () => {
     if (imageLink === IMG4) {
       alert("Kawa już zaparzona!")
-      console.log()
     } else if (imageLink === IMG2 || imageLink === IMG3) {
-      alert("Kawa już się parzy!")
+      alert("No przecież się parzy!")
     } else {
+      audioClip.map((soundObj, index) => {
+        return soundPlay(soundObj.sound)
+      })
     setImageLink(IMG2);
     setTimeout(() => {
       setImageLink(IMG3);
@@ -24,10 +40,11 @@ function App() {
     }, 2000);
     }
   };
-
+Howler.volume(1.0)
   return (
     <div className="App">
       <img src={imageLink} alt="Caffe Maker" onClick={handleClick} />
+      <h1 style={(imageLink === IMG4) ? {} : {display: "none"}}>Kawa gotowa. <br />Należy się 500 zł.</h1>
     </div>
   );
 }
--- src/Audio.js
+++
@@ -1,7 +0,0 @@
-import BUBBLES from "./audio/coffee-makers-bubbling-32827.mp3";
-
-const Audio = () => {
-    return  <audio id="bubbles" preload="true" autoPlay src={BUBBLES} />
-}
-
-export default Audio;
